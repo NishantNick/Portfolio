8429ac44ba715322ac559e07062360deb2ef96a7
NishantNick <nishantshre@gmail.com>
2016-09-19 01:53:42 +0530

    Files for master branch

---
+++ Scripts/home.js
@@ -0,0 +1,27 @@
+﻿
+$("#about").load("../Markup/about.html", function () {
+    //alert("about Loaded Successfuly.");
+}, function () {
+    alert("about Loading Failed");
+});
+$("#portfolio").load("../Markup/portfolio.html", function () {
+   // alert("portfolio Loaded Successfuly.");
+}, function () {
+    alert("portfolio Loading Failed");
+});
+$("#contact").load("../Markup/contact.html", function () {
+   // alert("contact Loaded Successfuly.");
+}, function () {
+    alert("contact Loading Failed");
+});
+
+$(".dropdown-toggle").on('click', function () {
+    $(this).find('.caret').toggleClass('active');
+});
+
+$("body").on('click', function (event) {
+    if ($(event.target).attr("class") == "dropdown-toggle") {
+        return;
+    }
+    $(".dropdown-toggle").find('.caret').removeClass('active');
+});
---
+++ Scripts/portfolio.js
@@ -0,0 +1,38 @@
+﻿$('#senteProjBtn').on('click', function () {
+
+    if ($('#senteProjBtn').text() == 'More Details') {
+        $('#senteProjParent').removeClass().addClass('col-xs-12');
+        $('#senteProjChild').removeClass('professionalProjects');
+        $('#senteProjBtn').text('Less Details');
+    } else {
+        $('#senteProjParent').removeClass().addClass('col-xs-12 col-sm-6 col-md-4');
+        $('#senteProjBtn').text('More Details');
+        $('#senteProjChild').addClass('professionalProjects');
+    }
+});
+
+$('#mspBtn').on('click', function () {
+
+    if ($('#mspBtn').text() == 'More Details') {
+        $('#mspParent').removeClass().addClass('col-xs-12');
+        $('#mspChild').removeClass('professionalProjects');
+        $('#mspBtn').text('Less Details');
+    } else {
+        $('#mspParent').removeClass().addClass('col-xs-12 col-sm-6 col-md-4');
+        $('#mspBtn').text('More Details');
+        $('#mspChild').addClass('professionalProjects');
+    }
+});
+
+$('#nbBtn').on('click', function () {
+
+    if ($('#nbBtn').text() == 'More Details') {
+        $('#nbParent').removeClass().addClass('col-xs-12');
+        $('#nbChild').removeClass('professionalProjects');
+        $('#nbBtn').text('Less Details');
+    } else {
+        $('#nbParent').removeClass().addClass('col-xs-12 col-sm-6 col-md-4');
+        $('#nbBtn').text('More Details');
+        $('#nbChild').addClass('professionalProjects');
+    }
+});
